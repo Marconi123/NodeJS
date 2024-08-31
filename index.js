e627b7640ce24378d126433c29bd6c2e18c35814
Vladislav <vladislavgalkin5@gmail.com>
2024-08-31 23:43:03 +0300

    test

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+// let result =  prompt("Какое «официальное» название JavaScript?");
+// function checkName (x){
+//         if (x == 'ECMAScript') {    
+//             alert('Верно!')
+//       } else {
+//         alert("«Не знаете? ECMAScript!")
+//      }
+// }
+// checkName (result);
+
+// Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert:
+
+// 1, если значение больше нуля,
+// -1, если значение меньше нуля,
+// 0, если значение равно нулю.
+
+// let number = prompt('Input your number');
+// function checkNumber (x){
+//     if (x > 0) {
+//         alert(1);
+//     } else if (x < 0) {
+//         alert(-1); 
+//     } else if (x == 0) {
+//         alert(0);
+//     }
+// }
+
+// checkNumber(number);
+
+// let message = (number > 0) ? "1" : (number < 0) ? "-1" : "0"; 
+// alert (message);
+
+// let result1;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+// let result = (a + b < 4) ? "Мало" : "Много";
+
+
+// let message;
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+// let login = prompt ('Your job title');
+// let message = (login == 'Сотрудник') ? 'Привет' : 
+// (login == 'Директор') ? 'Здравствуйте' : 
+// (login == '') ? 'Нет логина' : '';
+// alert (message);
